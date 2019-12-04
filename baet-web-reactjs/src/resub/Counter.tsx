ce28b29d7d366814b1815731feb0d5336971b3aa
Yilmaz Guleryuz <397125+zeusbaba@users.noreply.github.com>
2019-12-04 10:50:40 +0100

    wip.. start using typescript and resub

---
+++ baet-web-reactjs/src/resub/Counter.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { ReactElement } from 'react';
+import { ComponentBase } from 'resub';
+import CounterStore from './CounterStore';
+
+export interface CounterProps {}
+
+interface CounterState {
+    counter: number;
+}
+
+export class Counter extends ComponentBase<CounterProps, CounterState> {
+    protected _buildState() {
+        return {
+            counter: CounterStore.getCounter(),
+        };
+    }
+
+    public render(): ReactElement<any> {
+        return (
+            <>
+                <div>
+                    <h1>{ this.state.counter }</h1>
+                </div>
+
+                <div>
+                    <button onClick={() => CounterStore.increment()}>+</button>
+                    <button onClick={() => CounterStore.decrement()}>-</button>
+                    <button onClick={() => CounterStore.reset()}>Reset</button>
+                </div>
+            </>
+        );
+    }
+}
