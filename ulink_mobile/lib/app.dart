d27f3cfc7b254fd7d06a037a4f1fd5564e975c85
Yilmaz Guleryuz <397125+zeusbaba@users.noreply.github.com>
2020-02-17 20:08:49 +0100

    ulink_mobile works on real iPhone! :-)

---
+++ ulink_mobile/lib/app.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ulink_mobile/tabs/info_about.dart';
+import 'package:ulink_mobile/tabs/links_create.dart';
+import 'package:ulink_mobile/tabs/links_display.dart';
+import 'package:ulink_mobile/shared/api_login.dart';
+import 'package:flutter/services.dart';
+
+class LinkShortenerApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // This app is designed only to work vertically, so we limit
+    // orientations to portrait up and down.
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return CupertinoApp(
+      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
+      title: "uLINK.no URL Shortener",
+      home: LinkShortenerAppHome(),
+    );
+  }
+}
+
+class LinkShortenerAppHome extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    var deviceType = (Theme.of(context).platform == TargetPlatform.iOS)?"ios":"android";
+    ApiLogin.loadAppToken(deviceType);
+
+    return Scaffold(
+      // Appbar
+      appBar: AppBar(
+        leading: Image.asset('images/logo_icon.png'),
+        // Title
+        title: Text("uLINK.no :: shorten & simplify"),
+        // Set the background color of the App Bar
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.transform, size: 44.0),//(CupertinoIcons.add),
+              //title: Text('Link'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.link, size: 44.0),
+              //title: Text('Links'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info, size: 44.0),
+              //title: Text('Info'),
+            ),
+          ],
+        ),
+        tabBuilder: (context, index) {
+          var tabView;
+          switch (index) {
+            case 0:
+              tabView = CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                  child: LinksCreate(),
+                );
+              });
+              break;
+            case 1:
+              tabView = CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                  child: LinksDisplay(),
+                );
+              });
+              break;
+            case 2:
+              tabView = CupertinoTabView(builder: (context) {
+                return CupertinoPageScaffold(
+                  child: InfoAbout(),
+                );
+              });
+              break;
+          }
+          return tabView;
+        },
+      ),
+    );
+  }
+}
---
+++ ulink_mobile/lib/shared/api_login.dart
@@ -0,0 +1,177 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:device_info/device_info.dart';
+import 'package:package_info/package_info.dart';
+import 'package:ulink_mobile/shared/user.dart';
+import 'package:ulink_mobile/shared/app_config.dart';
+
+class ApiLogin {
+
+  static final AppConfig appConfig = AppConfig.getAppConfig('prod');
+
+  // If you call new MyUtils(), you'll always get the same instance.
+  //You need to import the file that contains class MyUtils {} everywhere where you want to use it.
+  //??= means new MyUtils._() is only executed when _instance is null and if it is executed the result will be assigned to _instance before it is returned to the caller.
+  static ApiLogin _instance;
+  factory ApiLogin() => _instance ??= new ApiLogin._();
+  ApiLogin._();
+
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  static Future<String> _getDeviceId(String deviceType) async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    if (deviceType.toLowerCase() == "ios") {
+      IosDeviceInfo iosDeviceInfo = await deviceInfo.iosInfo;
+      return iosDeviceInfo.identifierForVendor; // unique ID on iOS
+    } else {
+      AndroidDeviceInfo androidDeviceInfo = await deviceInfo.androidInfo;
+      return androidDeviceInfo.androidId; // unique ID on Android
+    }
+  }
+
+  static Future<Map<String, String>> _getPackageInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    Map<String, String> packageInfoMap = new Map();
+    packageInfoMap['appName'] = packageInfo.appName;
+    packageInfoMap['version'] = packageInfo.version;
+    packageInfoMap['packageName'] = packageInfo.packageName;
+    packageInfoMap['buildNumber'] = packageInfo.buildNumber;
+
+    return packageInfoMap;
+  }
+
+  static loadAppToken(String deviceType) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String appToken = "";
+
+    // FIXME: remove this after validation!
+    //prefs.clear();
+
+    if (prefs.containsKey(appConfig.appTokenKey)) {
+      //appToken = prefs.getString(appTokenKey);
+      print('appToken EXISTs in local, move on..!');
+    }
+    else {
+      // implement login+auth via uLINK API!
+      var userId = await _getDeviceId(deviceType);
+      userId = "uLINK-mobile-" + userId.toLowerCase();
+      var user = User(userid: userId, password: userId, createdAt: '');
+      print('appToken doesNOT exist in local, reloading via API! for userId: $userId');
+
+      bool isUserOk = await _createUser(user);
+
+      if (isUserOk) {
+
+        appToken = await _createUserToken(user);
+        if (appToken != '') {
+          await prefs.setString(appConfig.appTokenKey, appToken);
+        }
+        else {
+          print('ERROR occured when creating userToken! $appToken');
+        }
+      }
+      else {
+        print('ERROR occured when creating user! $user');
+      }
+    }
+    //print('appToken: $appToken');
+
+    return appToken;
+  }
+
+  static _createUser(User user) async {
+    bool isUserOk = false;
+
+    Map<String, String> packageInfoMap = await _getPackageInfo();
+    String appInfo = jsonEncode(packageInfoMap);
+    print('appInfo: $appInfo');
+
+    Map<String, dynamic> reqBody = new Map();
+    reqBody['userid'] = user.userid;
+    reqBody['password'] = user.password;
+    reqBody['extra'] = {
+      'appInfo': appInfo
+    };
+
+    //var httpClient = http.Client();
+    var apiResponse = await http.post(
+        appConfig.apiUrl+appConfig.apiEndpoint['users'],
+        body: json.encode(reqBody),
+        headers: {
+          HttpHeaders.contentTypeHeader: "application/json",
+          HttpHeaders.acceptHeader: "application/json"
+        }
+    );
+
+    Map jsonResponse = json.decode(apiResponse.body);
+    //print('jsonResponse: $jsonResponse');
+    if (isResponseOk(apiResponse.statusCode)
+      && jsonResponse.containsKey('userid')
+      ) {
+      isUserOk = true;
+    }
+    else if (apiResponse.statusCode == 500
+      && jsonResponse.containsKey("message")
+      ) {
+      // check if user existed already
+
+      // {
+      //    "name": "GeneralError",
+      //    "message": "E11000 duplicate key error collection: baet.baet_users index: _id_ dup key: { _id: \"flutter1\" }",
+      //    "code": 500,
+      //    "className": "general-error",
+      //    "data": {
+      //        "code": 11000
+      //    },
+      //    "errors": {}
+      //}
+      String errorMsg = jsonResponse['message'] as String;
+      if (errorMsg.contains("index: _id_ dup key")) {
+        isUserOk = true;
+      }
+    }
+    
+    return isUserOk;
+  }
+
+  static _createUserToken(User user) async {
+    String userToken = '';
+
+    Map<String, dynamic> reqBody = new Map();
+    reqBody['userid'] = user.userid;
+    reqBody['password'] = user.password;
+    reqBody['strategy'] = 'local';
+
+    //var httpClient = http.Client();
+    var apiResponse = await http.post(
+        appConfig.apiUrl+appConfig.apiEndpoint['token'],
+        body: json.encode(reqBody),
+        headers: {
+          HttpHeaders.contentTypeHeader: "application/json",
+          HttpHeaders.acceptHeader: "application/json"
+        }
+    );
+
+    if (isResponseOk(apiResponse.statusCode)) {
+
+      var jsonResponse = json.decode(apiResponse.body);
+      //print('AUTH jsonResponse: $jsonResponse');
+
+      if (jsonResponse.containsKey('accessToken')) {
+        userToken = jsonResponse['accessToken'] as String;
+        //print('AUTH appToken is OK: $appToken');
+      }
+    }
+
+    return userToken;
+  }
+
+  static isResponseOk(int statusCode) {
+    return (statusCode == 200 || statusCode == 201);
+  }
+
+}
+
+
